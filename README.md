b0c4a2c08606100834a829b3217b8f426ad6daf8
Jonathan <jmlacker@gmail.com>
2019-02-20 11:00:34 -0500

    Added dataFormatter.go file, and updated README.md to show usage.

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # Go-Format-Fix
 Takes just about any builtin datatype and returns a formatted string as per your specification.
+
+This is particularly useful for maintaining a consistent formatting for phone numbers, currencies, dates, and the like, but can be applied to any string that may need to be sanitized and formatted in a specific way.
+
+# Usage
+
+```
+package main
+
+// It's recommended to use an alias for the package,
+// simply because it makes it easier to refer to after importing it.
+
+import (
+  formatter "github.com/GammaWatt/go-format-fix"
+  "fmt"
+  )
+
+
+// The formatter function you define must always be of type
+// func (string) string
+
+func myFormatter(y string) string {
+  var runes []rune = []rune(y)
+  var formattedResult []rune
+
+  for i := range runes {
+    formattedResult = append(formattedResult, runes[i])
+    formattedResult = append(formattedResult, rune('^'))
+  }
+  return string(formattedResult[:len(formattedResult)-1])
+}
+
+// x is converted to a string, then passed to your formatting function.
+// It works with any width of int or float so you don't have to worry
+// about type casting.
+
+func testInt(x int8) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testUint(x uint64) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testF32(x float32) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testF64(x float64) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testByte(x []byte) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testRune(x []rune) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func testString(x string) string {
+  return formatter.Format(x, myFormatter)
+}
+
+func main() {
+  fmt.Println(testInt(int8(127)))
+  fmt.Println(testUint(uint64(18446744073709551615)))
+  // f32 only has 8-point precision... you'll get errors beyond that
+  fmt.Println(testF32(float32(3.1415926)))
+  // f64 only has 16-point precision... you'll get errors beyond that
+  fmt.Println(testF64(float64(2.7182818284590459)))
+  fmt.Println(testByte([]byte{'a','b','c','d','e','f','g'}))
+  fmt.Println(testRune([]rune{'א','ב','ג','ד','ה','ו','ז'}))
+  fmt.Println(testString(string("There be dragons")))
+}
+```
+
+###### output
+
+```
+1^2^7
+1^8^4^4^6^7^4^4^0^7^3^7^0^9^5^5^1^6^1^5
+3^.^1^4^1^5^9^2^5
+2^.^7^1^8^2^8^1^8^2^8^4^5^9^0^4^6
+a^b^c^d^e^f^g
+א^ב^ג^ד^ה^ו^ז
+T^h^e^r^e^ ^b^e^ ^d^r^a^g^o^n^s
+```
---
+++ dataFormatter.go
@@ -0,0 +1,164 @@
+package dataFormatter
+
+import (
+  "strconv"
+  "reflect"
+  "strings"
+)
+
+func normalizeToUint64(x interface{}) uint64 {
+  ui8ToUi64 := func (x interface{}) uint64 {
+    r := x.(uint8)
+    var f uint64
+    f = uint64(r)
+    return f
+  }
+
+  ui16ToUi64 := func (x interface{}) uint64 {
+    r, l := x.(uint16)
+  	if !l {
+      return ui8ToUi64(x)
+  	} else {
+  	 var f uint64
+     f = uint64(r)
+     return f
+  	}
+  }
+
+  ui32ToUi64 := func (x interface{}) uint64 {
+    r, l := x.(uint32)
+  	if !l {
+  	 return ui16ToUi64(x)
+  	} else {
+  	 var f uint64
+  	 f = uint64(r)
+     return f
+   }
+ }
+
+  ui64ToUi64 := func (x interface{}) uint64 {
+    r, l := x.(uint64)
+  	if !l {
+  	 return ui32ToUi64(x)
+  	} else {
+     return r
+  	}
+  }
+
+  r, l := x.(uint)
+	if !l {
+	 return ui64ToUi64(x)
+	} else {
+	 var f uint64
+	 f = uint64(r)
+   return f
+  }
+}
+
+
+func normalizeToInt64(x interface{}) int64 {
+  i8Toi64 := func (x interface{}) int64 {
+    r := x.(int8)
+    var f int64
+    f = int64(r)
+    return f
+  }
+
+  i16Toi64 := func (x interface{}) int64 {
+    r, l := x.(int16)
+  	if !l {
+      return i8Toi64(x)
+  	} else {
+  	 var f int64
+     f = int64(r)
+     return f
+  	}
+  }
+
+  i32Toi64 := func (x interface{}) int64 {
+    r, l := x.(int32)
+  	if !l {
+  	 return i16Toi64(x)
+  	} else {
+  	 var f int64
+  	 f = int64(r)
+     return f
+   }
+ }
+
+  i64Toi64 := func (x interface{}) int64 {
+    r, l := x.(int64)
+  	if !l {
+  	 return i32Toi64(x)
+  	} else {
+     return r
+  	}
+  }
+
+  r, l := x.(int)
+	if !l {
+	 return i64Toi64(x)
+	} else {
+	 var f int64
+	 f = int64(r)
+   return f
+  }
+}
+
+func normalizeDataType(x interface{}) string {
+  if reflect.TypeOf(x).String()[:3] == "int" {
+    y := strconv.FormatInt(normalizeToInt64(x), 10)
+    return y
+  } else if reflect.TypeOf(x).String()[:4] == "uint" {
+    y := strconv.FormatUint(normalizeToUint64(x), 10)
+    return y
+  } else if dataType := reflect.TypeOf(x).String(); dataType[:5] == "float" {
+    bitSize, _ := strconv.Atoi(dataType[5:7])
+    var y string
+    if bitSize == 64 {
+      y = strconv.FormatFloat(x.(float64), 'f', -1, bitSize)
+    } else {
+      // strconv only accepts float64 arg
+      y = strconv.FormatFloat(float64(x.(float32)), 'f', -1, bitSize)
+    }
+    return y
+  } else if reflect.TypeOf(x).String() == "string" {
+    y := x.(string)
+    return y
+    // []byte is evaluates as []uint8 by reflect
+  } else if reflect.TypeOf(x).String() == "[]uint8" {
+    y := string(x.([]byte))
+    return y
+    // []rune is evaluated as []int32 by reflect
+  } else if reflect.TypeOf(x).String() == "[]int32" {
+    y := string(x.([]rune))
+    return y
+  } else {
+    panic(
+      reflect.ValueOf(x).String() +
+      " must be of type float, int, uint string, []byte, or []rune...")
+  }
+}
+
+func removeChar(x string, char byte) string {
+  return strings.Join(strings.Split(x, string(char)), "")
+}
+
+// Takes any chars found in string "chars" out of string "x"
+func RemoveCharsInString(x, chars string) string {
+  y := x
+  for i := range chars {
+    y = removeChar(y, byte(chars[i]))
+  }
+  return y
+}
+
+type formatterFunction func(string) string
+
+func Format(x interface{}, y formatterFunction) string {
+  n := normalizeDataType(x)
+  return y(n)
+}
+// Format takes x as string and plugs it into y to be formatted as you wish
+// always returns a string as formatted by y
+// Acceptable types are any float, any int, any uint, string, []byte, or []rune.
